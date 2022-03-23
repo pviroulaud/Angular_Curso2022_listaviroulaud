39ea6ec8bf498951ba2232dd6224776962ce2e28
pviroulaud <93263713+pviroulaud@users.noreply.github.com>
2022-03-23 10:29:53 -0300

    maquetado

--- listaviroulaud/src/app/app.module.ts
+++ listaviroulaud/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { ContentComponent } from './componentes/content/content.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
